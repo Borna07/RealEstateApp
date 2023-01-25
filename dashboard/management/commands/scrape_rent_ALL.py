1cf4b7dcbdbcae47a1c0b189d661ad4c0e18cfef
Borna Molnar-Erhatic <borna.molnar-erhatic@gmx.at>
2023-01-25 14:38:23 +0100

    SCRAPE_ALL

--- dashboard/management/commands/scrape_rent_ALL.py
+++ dashboard/management/commands/scrape_rent_ALL.py
@@ -18,11 +18,14 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         for city in Info.keys():
+            # get the start time
+            st = time.time()
 
             city_name = city
 
             url = Info[city_name][2]
             ime = Info[city_name][1]
+
 
 
             #find all pages
@@ -43,14 +46,14 @@ class Command(BaseCommand):
             #check raw entries, save raw dataframe
             dj_raw_entries = len(df.index)
             
-            full_ime_raw = str(current_date) + "_RAW" + ime + ".xlsx"
+            full_ime_raw = str(current_date) + "_RAW" + ime + "_RENT" + ".xlsx"
             raw_path =  MEDIA_ROOT + "/" + full_ime_raw
             dj_df_raw  = df.to_excel(raw_path)
 
             #clean dataframe
-            df = dataframe_cleaner(df)
+            df = dataframe_cleaner_rent(df)
 
-            full_ime =  str(current_date) + ime + ".xlsx"
+            full_ime =  str(current_date) + ime + "_RENT" + ".xlsx"
             clean_path = MEDIA_ROOT + "/" + full_ime
             dj_df = df.to_excel(clean_path)
             dj_clean_entries = len(df.index)
@@ -72,23 +75,29 @@ class Command(BaseCommand):
 
             min_per_sqr_df = df[df['€/m²'] == df['€/m²'].min()]
             min_per_sqr = [min_per_sqr_df['€/m²'].values[0], min_per_sqr_df['Link'].values[0]]
-
             med_sale_price = df["Cijena"].mean()
 
             time_now = timezone.now()
 
-
-
-
-
-            Document.objects.create(
+            Rents.objects.create(
                 document = full_ime, document_raw = full_ime_raw, uploaded_at = time_now,
                 calendar_week = time_now.isocalendar()[1] , raw_entries = dj_raw_entries, clean_entries = dj_clean_entries,
-                city = city_name, avg_price_sqrm = dj_avg_price_sqrm, avg_size = dj_avg_size,
+                city = city_name, avg_price_sqrm = dj_avg_price_sqrm, avg_price_sqrm_decimal= dj_avg_price_sqrm, avg_size = dj_avg_size,
                 avg_year = dj_avg_year, highest_price = max[0], highest_price_link = max[1],
-                highest_price_sqrm = max_per_sqr[0], highest_price_sqrm_link = max_per_sqr[1],
-                lowest_price = min[0], lowest_price_link = min[1], lowest_price_sqrm = min_per_sqr[0],
+                highest_price_sqrm = max_per_sqr[0],highest_price_sqrm_decimal = max_per_sqr[0], highest_price_sqrm_link = max_per_sqr[1],
+                lowest_price = min[0], lowest_price_link = min[1], lowest_price_sqrm = min_per_sqr[0],lowest_price_sqrm_decimal = min_per_sqr[0],
                 lowest_price_sqrm_link = min_per_sqr[1], avg_price = med_sale_price
             )
+
+            # # get the end time
+            # et = time.time()
+
+            # # get the execution time
+            # elapsed_time = et - st
+            # elapsed_time_min = elapsed_time/60
+            # print('Execution time:', elapsed_time_min, 'minutes')
+            # print('%s added' % (city_name))
+            # self.stdout.write( 'Rents job complete' )
+
 
 
